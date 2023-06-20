254d8e19515fe0b598c5adb6b6d19c676eda1c34
Neuron-Logic <137006850+Neuron-Logic@users.noreply.github.com>
2023-06-20 04:39:38 +0300

    Add files via upload

--- train.js
+++ train.js
@@ -1,28 +1,30 @@
 'use strict';
 
-var TrueM = [],
-    Solve = [],
-    Solve_Y,
+var TrueM = [],  //Набор данных для обучения
+    Solve = [],  //Пример для решения
+    Solve_Y,  //Результат решения приера
     W = [], //веса на входе
     W_out = [], //веса на выходе
     X_in = 2,  //входные
     Y_out = 1,  //выходные
-    H,  //нейроны в скрытом слое
-    H1, Y_H1 = [], Yi_out = [], H_out = [],
-    error = [],
+    H,  //Кол-во нейронов в скрытом слое
+    H_out = [],  //Массив обработанных весов
+    H1, //промежуточная переменная для весов
+    Y_H1 = [], //Результаты перед выходным нейроном
+    Yi_out = [], //Результаты после выходного нейрона
+    error = [],  // Массив ошибок
     D = [], D_out, //градиенты
     n,  //скорость
     era,  //эпоха
-    error_,
-    perfect_error,
-    x, count = 0;
+    error_,  //Сумма ошибок
+    perfect_error,  //Минимальная ошибка
+    x, count = 0; //Прочее
 
 
 
-function sigmoida(a) {
-    return 1 / (1 + Math.exp(-1 * a))
-}
-function input(){
+
+
+function input(){ //Ввод параметров обучения
     n = document.getElementById('n').value;
     era = document.getElementById('era').value;
     perfect_error = document.getElementById('perfect_error').value;
@@ -39,7 +41,8 @@ function input(){
 }
 
 
-function training(){
+function training(){  //Кнопка "Обучить"
+    
     TrueM = [[document.getElementById('01').value, document.getElementById('02').value, document.getElementById('03').value],
     [document.getElementById('11').value, document.getElementById('12').value, document.getElementById('13').value],
     [document.getElementById('21').value, document.getElementById('22').value, document.getElementById('23').value],
@@ -54,29 +57,7 @@ function training(){
     alert('Обучение завершено!')
 }
 
-function solve(){
-
-    Solve = [document.getElementById('input1').value, document.getElementById('input2').value]
-
-    //Решение
-        for (var i = 0; i < H; i++) {
-            H1 = (Solve[0] * W[i * 2]) + (Solve[1] * W[i * 2 + 1]);
-            Y_H1[i] = 1 / (1 + Math.exp(-1 * H1));
-        }
-
-        for (var j = 0; j < W_out.length; j++) {
-            H_out[j] = (W_out[j] * Y_H1[j])
-        }
-
-        Solve_Y = 1 / (1 + Math.exp(-1 * H_out.map(i => x += i, x = 0).reverse()[0]));
-
-    document.getElementById('output').innerHTML = Solve_Y.toFixed(4);
-  
-}
-
-    //обучение
-function train(){
-
+function train(){  //Обучение
 
     for (var e = 0; e < era; e++) {
         for (var str = 0; str < TrueM.length; str++) {
@@ -114,6 +95,25 @@ function train(){
     }
 }
 
+function solve(){  //Решение примера
+
+    Solve = [document.getElementById('input1').value, document.getElementById('input2').value]
+
+    
+        for (var i = 0; i < H; i++) {
+            H1 = (Solve[0] * W[i * 2]) + (Solve[1] * W[i * 2 + 1]);
+            Y_H1[i] = 1 / (1 + Math.exp(-1 * H1));
+        }
+
+        for (var j = 0; j < W_out.length; j++) {
+            H_out[j] = (W_out[j] * Y_H1[j])
+        }
+
+        Solve_Y = 1 / (1 + Math.exp(-1 * H_out.map(i => x += i, x = 0).reverse()[0]));
+
+    document.getElementById('output').innerHTML = Solve_Y.toFixed(4);
+  
+}
 
 
 
